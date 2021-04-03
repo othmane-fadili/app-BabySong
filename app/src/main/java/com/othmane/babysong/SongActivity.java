2942b62bb7cbeb6316468f07a4979726326bbaa1
othmane-fadili <othmanfadili20@gmail.com>
2021-04-03 16:48:41 +0200

    Initialization of project

---
+++ app/src/main/java/com/othmane/babysong/SongActivity.java
@@ -0,0 +1,14 @@
+package com.othmane.babysong;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SongActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_song);
+    }
+}
